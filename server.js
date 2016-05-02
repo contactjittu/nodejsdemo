469f1588176f0da37d41c36b0df5e8def238a38e
Jitendra Kumar <contactjittu@gmail.com>
2016-05-02 14:52:42 +0530

    Updating server.js for cors

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var bodyParser = require('body-parser');
 var logger = require('morgan');
+var cors = require('cors')
 mongoose.connect('mongodb://localhost:27017/mean');
 
 module.exports = mongoose.connection;
@@ -14,24 +15,7 @@ app.get('/', function (req, res) {
 })
  
 // Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use('cors');
 
 app.use(logger('dev')); 
 app.use(bodyParser.json());
