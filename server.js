3534a9c4ce08b03e54d4941bfa0113bfd4b05627
Jitendra Kumar <jitendrak@anomaly.co.in>
2016-06-08 18:16:00 +0530

    rename saveFile.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 var erc = require('express-route-controller');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var bodyParser = require('body-parser');
 var logger = require('morgan');
+var compression = require("compression");
+var helmet = require('helmet')
+var cors = require("cors");
 mongoose.connect('mongodb://localhost:27017/mean');
 
 module.exports = mongoose.connection;
@@ -12,31 +15,14 @@ module.exports = mongoose.connection;
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
- 
-// Add headers
-app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
-app.use(logger('dev')); 
+app.use(helmet());
+app.use(cors());
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser({limit: '115mb'}));
+app.use(bodyParser.urlencoded({ extended: true },{limit: '115mb'}));
+app.use(compression());
 
 app.listen(3000, function() {
   console.log('Express server listening on port ' + 3000);
@@ -45,9 +31,10 @@ app.listen(3000, function() {
 erc(app, {
     controllers: __dirname + '/routes',
     routes: {
-    	'/getEntity': {action: 'saveFile#getEntity',method:'get'},
-        '/saveEntity': { action: 'saveFile#saveEntity', method: 'post'},
-        '/updateEntity': { action: 'saveFile#updateEntity', method: 'post'},
-        '/removeEntity': { action: 'saveFile#removeEntity', method: 'delete'}
+    	'/shoturl': {action: 'blog#shoturl',method:'get'},
+    	'/getEntity': {action: 'blog#getEntity',method:'get'},
+        '/saveEntity': { action: 'blog#saveEntity', method: 'post'},
+        '/updateEntity': { action: 'blog#updateEntity', method: 'post'},
+        '/removeEntity': { action: 'blog#removeEntity', method: 'delete'}
     }
 });
